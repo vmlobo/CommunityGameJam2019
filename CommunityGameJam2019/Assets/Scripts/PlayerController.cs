acc30a724e9e557b9d894cea5da4a7af0a41915d
Vitor Lobo <vitormarqueslobo@gmail.com>
2019-08-25 03:54:12 +0100

    Flashlight and movement

---
+++ CommunityGameJam2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+    private Collider2D playerCollider;
+    private Transform flashlight;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        playerCollider = GetComponent<CircleCollider2D>();
+        flashlight = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += movement * Time.deltaTime * speed;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //crosshair.position = new Vector3(mousePos.x, mousePos.y, -5);
+        //flashlight_transform.up = (crosshair.position - flaslight.position);
+
+
+    }
+
+}
