a9f082205109d4626f1a4a1e814e7a603d9023c6
Vitor Lobo <vitormarqueslobo@gmail.com>
2019-08-25 21:35:37 +0100

    Enemy movement

--- CommunityGameJam2019/Assets/Scripts/EnemyController.cs
+++ CommunityGameJam2019/Assets/Scripts/EnemyController.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyControllerScript : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
+
+    private SpriteRenderer sr; //TODO flip sprite on mov.x<0
+    public Transform player;
+
+    private bool inSight;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inSight = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!inSight)
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "LightCone")
+        {
+            inSight = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "LightCone")
+        {
+            inSight = false;
+        }
+
+    }
+
 }
