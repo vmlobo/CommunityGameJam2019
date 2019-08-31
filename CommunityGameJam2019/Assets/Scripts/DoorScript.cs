6622d3acbf2a85e10baa4481007714a1132fc87a
Vitor Lobo <vitormarqueslobo@gmail.com>
2019-08-31 17:59:56 +0100

    Started doing levels

---
+++ CommunityGameJam2019/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorScript : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+    private Transform text;
+
+
+    private void Start()
+    {
+        text = transform.GetChild(0);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<PlayerController>().hasKeyBool())
+            {
+                //SceneManager.LoadScene(newLevel);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); //TODO testar e fazer if p ultimo nivel
+            }
+            else
+            {
+                StartCoroutine(PopText());
+            }
+        }
+    }
+
+    IEnumerator PopText()
+    {
+        text.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.6f);
+        text.gameObject.SetActive(false);
+    }
+
+}
